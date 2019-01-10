c0c20eb712f5eab9b59e375c4b55ed8e61960bd1
NĐHậu <nguyendinhhau1807@gmail.com>
2019-01-10 17:57:49 +0700

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = '3e9166ad629eca6587a5e501e4e30961';
+
+// 208af478a5c65b70f94de45e227f5c51
+
+// d3ab033003c2e546e131f5b45402e3e9
+
+// 3e9166ad629eca6587a5e501e4e30961
